78723dd836335722ec5fc83601435d5a82ff9944
hosokawahiyori <h.hosokawa.sys20@morijyobi.ac.jp>
2021-03-30 13:03:19 +0900

    コミット面積のやつ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def top():
     return render_template("index.html")
 
-@app.route("/kyuyo")
-def kyuyo():
-    return render_template("kyuyo.html")
-
 @app.route("/en")
 def en():
     return render_template("en.html")
+
+@app.route("/hankei")
+def hankei():
+    han = request.args.get("han")
+    han = int(han)
+    menseki = han * han * 3.14
+    ensyu = 2 * han * 3.14
+    return render_template("hankei.html", menseki=menseki, ensyu=ensyu)
 
 if __name__ == "__main__":
     app.run(debug=True)
